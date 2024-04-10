d12b0a6748a586bc58150f7aea746a6b2e67f87a
Jason Mei <s0922380@gmail.com>
2024-04-10 13:32:38 +0800

    fix error

--- js/case.js
+++ js/case.js
@@ -1,35 +1,22 @@
 const global_cases = {
   cases: [
     {
-      name: "ES Plus營運管理系統",
+      name: "趨勢科技 - 雲端成本管理平台",
+      img: "/caselogo/trendmicro-ogsocial.jpg",
+      logo: "/caselogo/trendmicro-ogsocial.jpg",
+      techs: ["Vue", "ASP.NET", "Cosmos DB", "Azure"],
+    },
+    {
+      name: "依斯博萊斯活動方案有限公司 - 營運管理系統",
       img: "/caselogo/esplus-1.png",
       logo: "/caselogo/esplus.png",
-      techs: [
-        "Angular",
-        "ASP.NET",
-        "SQL Server",
-        "Bootstrap",
-        "AWS EC2",
-        "AWS S3",
-      ],
+      techs: ["Angular", "ASP.NET", "SQL Server", "Bootstrap", "AWS"],
     },
     {
       name: "愛妮島GO",
       img: "/caselogo/elnidogo-1.png",
       logo: "/caselogo/elnidogo.webp",
       techs: ["Flutter", "iOS", "Android"],
-    },
-    {
-      name: "昆龍貨運派車管理系統",
-      img: "/caselogo/kunglong-1.png",
-      logo: "/caselogo/kunglong.png",
-      techs: ["React", "NodeJs", "PostgreSQL", "Bootstrap"],
-    },
-    {
-      name: "許晉婚禮攝影",
-      img: "/caselogo/syuchin-1.png",
-      logo: "/caselogo/syuchin.png",
-      techs: ["PHP", "mySQL", "Wordpress"],
     },
   ],
 };
